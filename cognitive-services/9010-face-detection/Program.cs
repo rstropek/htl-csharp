48f1eb259da41237b2b9680aea30c3ceccea34c6
Rainer Stropek <rainer@software-architects.at>
2017-11-07 11:59:30 +0100

    Add Cognitive Services exercise

---
+++ cognitive-services/9010-face-detection/Program.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing.Pens;
+using SixLabors.Primitives;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FaceRecognition
+{
+    static class Program
+    {
+        const string subscriptionKey = "9f6.............................";
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.Error.WriteLine("Missing arguments. Please specify source and target file name.");
+                return;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                Console.Error.WriteLine($"Invalid argument. '{args[0]}' does not exist.");
+                return;
+            }
+
+            var imageBytes = ReadImageAsByteArrayAsync(args[0]).Result;
+            var faces = PerformFaceDetectionAsync(imageBytes).Result;
+            using (var image = AddFaceRectangles(imageBytes, faces))
+            {
+                image.SaveAsJpeg(File.OpenWrite(args[1]));
+            }
+        }
+
+        static async Task<byte[]> ReadImageAsByteArrayAsync(string imageFilePath)
+        {
+            using (var fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                var bytes = new byte[fileStream.Length];
+                await fileStream.ReadAsync(bytes, 0, bytes.Length);
+                return bytes;
+            }
+        }
+
+        static async Task<JArray> PerformFaceDetectionAsync(byte[] imageBytes)
+        {
+            // Read the Web API Reference Documentation for the face detection API
+            // at https://docs.microsoft.com/en-us/azure/cognitive-services/face/apireference
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+                var uri = uriBase + "?returnFaceId=true";
+                using (var content = new ByteArrayContent(imageBytes))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                    var response = await client.PostAsync(uri, content);
+                    var contentString = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<JArray>(contentString);
+                }
+            }
+        }
+
+        static Image<Rgba32> AddFaceRectangles(byte[] imageBytes, JArray faces)
+        {
+            var image = Image.Load<Rgba32>(imageBytes);
+            var pen = new Pen<Rgba32>(Rgba32.Red, 5);
+
+            image.Mutate(ctx =>
+            {
+                foreach (dynamic face in faces)
+                {
+                    dynamic faceRect = face.faceRectangle;
+                    var rect = new
+                    {
+                        left = (float)faceRect.left,
+                        top = (float)faceRect.top,
+                        right = (float)(faceRect.left + faceRect.width),
+                        bottom = (float)(faceRect.top + faceRect.height)
+                    };
+
+                    ctx.DrawPolygon(pen, new[]
+                    {
+                            new PointF(rect.left, rect.top), new PointF(rect.right, rect.top),
+                            new PointF(rect.right, rect.bottom), new PointF(rect.left, rect.bottom),
+                            new PointF(rect.left, rect.top)
+                    });
+                }
+            });
+
+            return image;
+        }
+    }
+}
---
+++ cognitive-services/9010-face-detection/readme.md
@@ -0,0 +1,66 @@
+# "Face Detection" Exercise
+
+## Introduction
+
+Your job is to write a command line tool that can detect faces in photos and mark them with a red rectangle. You should use a machine learning (aka *Artificial Intelligence* aka *Deep Learning*) approach to solve this problem.
+
+Of course, developing your own machine learning solution for face detection would take far too much time. Therefore, you should use Microsoft's [Cognitive Services](https://azure.microsoft.com/en-us/services/cognitive-services/). They offer various Web APIs with which you can interact with powerful machine learning systems for vision, speech, search, etc.
+
+## Specification
+
+* Write a platform-independent command line tool using C# and .NET Core.
+
+* The tool expects two parameters in the command line (example: `FaceRecognition c:\temp\input.jpg c:\temp\output.jpg`):
+  * source image's file name
+  * output image's file name
+
+* In case of missing or invalid arguments, the tool has to print a corresponding error message to *stderr*.
+
+* The tool has to...
+  * ...read the source file from disk, 
+  * ...detect faces, 
+  * draw a red rectangle around each face, and 
+  * save the resulting image into the output file.
+
+## Hints
+
+* **Before you start coding**, take some time to make yourself familiar with the services that [Microsoft Cognitive Services](https://azure.microsoft.com/en-us/services/cognitive-services/) offer. You can use the offering's website to interactively try the different machine learning services.
+
+* Next, create a **free** trial API key for [Microsoft Cognitive Services](https://azure.microsoft.com/en-us/services/cognitive-services/). It is free. The number of requests you can make is limited but sufficient for this exercise.
+
+* For platform-independent image manipulation, you can use the NuGet package `SixLabors.ImageSharp.Drawing`.
+
+* For handling JSON, you can use the NuGet package `Newtonsoft.Json`.
+
+* Use .NET's `FileStream.ReadAsync` method to read a binary image file into a byte array (`byte[]`).
+
+* Use .NET's `HttpClient.PostAsync` method to send your source image to *Cognitive Services* for face detection.
+
+* Take a look at the [Web API Reference Documentation](https://docs.microsoft.com/en-us/azure/cognitive-services/face/apireference) for the face detection API. If necessary, use a search engine to find a C# sample for face detection in Microsoft's documentation.
+
+* Here is an example for how to draw a red rectangle with *ImageSharp*:
+
+```
+var image = Image.Load<Rgba32>(imageBytes);
+var pen = new Pen<Rgba32>(Rgba32.Red, 5);
+
+image.Mutate(ctx =>
+{
+    ctx.DrawPolygon(pen, new[]
+    {
+        new PointF(100, 100), new PointF(300, 100),
+        new PointF(300, 300), new PointF(100, 300),
+        new PointF(100, 100)
+    });
+});
+
+image.SaveAsJpeg(targetFileName);
+```
+
+## Advanced Exercises
+
+* If you have finished the exercise early, try to get additional data from the face detection API (e.g. estimated age) and add it as text to the target image.
+
+## Solution
+
+You can find a sample solution (deliberately without comments) in [*Program.cs*](Program.cs) and [*FaceRecognition.csproj*](FaceRecognition.csproj). However, before you take a look at it, try to find your own solution!
