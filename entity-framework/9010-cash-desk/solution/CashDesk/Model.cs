206dc236fbec9032d77eff996bd8dbc2fae8906a
Rainer Stropek <rainer@software-architects.at>
2019-10-31 16:38:12 +0100

    Small fix

--- entity-framework/9010-cash-desk/solution/CashDesk/Model.cs
+++ entity-framework/9010-cash-desk/solution/CashDesk/Model.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
+
+// NOTE data annotations for class members
 
 namespace CashDesk
 {
